ecd453ec6585cfeb8e0fc649d62eacb86643eb23
lonfa1330100 <lonfa1330100@etu.cejepjonquiere.ca>
2019-11-01 10:49:12 -0400

    fini

--- Laboratoire2/Program.cs
+++ Laboratoire2/Program.cs
@@ -232,169 +232,177 @@ namespace Laboratoire2
         static void Main(string[] args)
         { 
             Carte[] tabCarte = new Carte[52];
-            //bool partie = true;
+            bool nouvellePartie = true, nouvelleManche = true, nouveauTour, dernierTour;
             Joueur joueur1,joueurPC;
-            Carte initCarte = new Carte(0,0,0);  //pour initialiser mes joueurs
-            int maxTableau;
-            int carteRetournee;
-            int carteChanger;
-            bool nouveauTour=true;
-            int nombreTypeCarte;
 
-            
+            Carte initCarte = new Carte(0,0,0);  //pour initialiser mes joueurs
+            int maxTableau, carteRetournee, carteChanger, nombreTypeCarte;
             joueur1 = new Joueur(initCarte, initCarte, initCarte);
             joueurPC = new Joueur(initCarte, initCarte, initCarte);
-
-            
-            InitialierTabCarte(ref tabCarte);
-            maxTableau = 51;
-            
-            DistribuerCarte(ref joueur1, ref joueurPC, ref tabCarte, ref maxTableau);
-            
-            carteRetournee = Convert.ToInt32(generateurNb.Next(0, maxTableau + 1));
-            PermuterCarte(ref tabCarte, carteRetournee, maxTableau);
-            carteRetournee = maxTableau;
-            maxTableau--;
-            
             do
             {
-                Console.WriteLine(" LES CARTES DE JOUEUR1 : ");
-                for (int i = 0; i < 3; i++)
-                {
-                    Console.WriteLine(i + 1 + " - sorte : " + joueur1.mesCartes[i].sorteCarte.ToString() + " valeur : " + joueur1.mesCartes[i].valeurJeu);
-                }
-                Console.WriteLine(" LES CARTES DE JOUEURPC : ");
-                for (int i = 0; i < 3; i++)
-                {
-                    Console.WriteLine(i + 1 + " - sorte : " + joueurPC.mesCartes[i].sorteCarte.ToString() + " valeur : " + joueurPC.mesCartes[i].valeurJeu);
-                }
-                Console.WriteLine(" Carte retournee sorte : " + tabCarte[carteRetournee].sorteCarte.ToString() + " valeur : " + tabCarte[carteRetournee].valeurJeu);
-                int choix;
 
-                
-
-                Console.WriteLine("Que voulez vous faire : \n 1 - prendre une nouvelle carte \n 2 - Prendre la carte retournee \n 3 - Fin de cette manche  (Vous de avoir une jeu qui vaut au moins 21 !!!)");
-                choix = Convert.ToInt32(Console.ReadLine());
-                switch (choix)
+                dernierTour = false;
+                nouveauTour = true;
+                InitialierTabCarte(ref tabCarte);
+                maxTableau = 51;
+                DistribuerCarte(ref joueur1, ref joueurPC, ref tabCarte, ref maxTableau);
+                carteRetournee = Convert.ToInt32(generateurNb.Next(0, maxTableau + 1));
+                PermuterCarte(ref tabCarte, carteRetournee, maxTableau);
+                carteRetournee = maxTableau;
+                maxTableau--;
+                do
                 {
-                    
-                    case 1:
-                        {
-                            Console.WriteLine("Quelle carte souhaitez vous changer 1, 2 ou 3 : ");
-                            carteChanger = Convert.ToInt32(Console.ReadLine());
-                            ChangerCarte(ref tabCarte, carteChanger - 1, ref joueur1, ref maxTableau);
-                            break;
-                        }
-                    case 2:
-                        {
-                            Console.WriteLine("Quelle carte souhaitez vous changer 1, 2 ou 3 : ");
-                            carteChanger = Convert.ToInt32(Console.ReadLine());
-                            ChangerCarteRetournee(ref tabCarte, carteChanger - 1, ref joueur1, ref maxTableau, ref carteRetournee);
-                            break;
-                        }
-                    case 3:
-                        {
-                            Console.WriteLine("Votre jeu de carte vaut : " + CalculerValeurCartes( joueur1));
-                            if (CalculerValeurCartes(joueur1) < 21)
-                                Console.WriteLine("votre jeu ne vaut pas au moins 21, vous ne pouvez pas mettre fin a ce tour  ");
-                            else
-                            {
-                                Console.WriteLine("Fin de ce tour  ");
-                                nouveauTour = false;
-                            }
-                            break;
-                        }
-                    default: break;
-                }
-                //Au tour de l'ordinateur de jouer
-                if (true) //CalculerValeurCartes(tabCarte, joueurPC) < 21
-                {
-                    nombreTypeCarte = CalculerNombreTypeCarte(joueurPC);
-                   
-                    switch (nombreTypeCarte)
+                    Console.WriteLine(" VOS CARTES : ");
+                    for (int i = 0; i < 3; i++)
                     {
-                        case 1: //Les trois carte du PC sont de meme sorte...
-                        {
-                            int plusPetiteCarte = DeterminerPlusPetiteCarte(joueurPC);
-                            int autreCarte1 = 1, autreCarte2 = 2;
-                            Console.WriteLine("plus petite carte " + plusPetiteCarte);
-                            Console.WriteLine("Nombre Carte PC " + nombreTypeCarte);
-                            if (plusPetiteCarte == 1)
-                            {
-                                autreCarte1 = 0;
-                                autreCarte2 = 2;
-                            }
-                            else if (plusPetiteCarte == 2)
-                            {
-                                autreCarte1 = 0;
-                                autreCarte2 = 1;
-                            }
-                            if (tabCarte[carteRetournee].sorteCarte == joueurPC.mesCartes[autreCarte1].sorteCarte || tabCarte[carteRetournee].sorteCarte == joueurPC.mesCartes[autreCarte2].sorteCarte)
-                                ChangerCarteRetournee(ref tabCarte, plusPetiteCarte,ref joueurPC, ref maxTableau, ref carteRetournee);
-                            else
-                                ChangerCarte(ref tabCarte, plusPetiteCarte, ref joueurPC, ref maxTableau);
-                            break;
-                        }
-                            
-                        case 2:
-                            Console.WriteLine("Nombre Carte PC " + nombreTypeCarte);
-                            
-                            Sorte sorte2Cartes = Sorte2Cartes(joueurPC), sorte1Carte = Sorte1Carte(joueurPC);
-                            int valeur2Cartes = ValeurCartes(joueurPC, sorte2Cartes), valeur1Carte = ValeurCartes(joueurPC, sorte1Carte);
-                            int index1Carte =Index1Carte(joueurPC, sorte1Carte), index2Cartes =Index2Cartes(joueurPC, sorte2Cartes) ;
-                            Console.WriteLine("Sorte 2 Carte : " + sorte2Cartes + " valeur 2 cartes : " + valeur2Cartes);
-                            Console.WriteLine("Sorte 1 Carte : " + sorte1Carte + " valeur 1 cartes : " + valeur1Carte);
-                            Console.WriteLine("Index 1 Carte : " + index1Carte + " Index 2 Cartes : " + index2Cartes);
-                            if (valeur2Cartes > valeur1Carte)
-                            {
-                                if(sorte2Cartes == tabCarte[carteRetournee].sorteCarte)
-                                    ChangerCarteRetournee(ref tabCarte, index1Carte, ref joueurPC, ref maxTableau, ref carteRetournee);
-                                
-                                else
-                                    ChangerCarte(ref tabCarte, index1Carte, ref joueurPC, ref maxTableau);
-                                
-                            }
-                            else
-                            {
-                                if (tabCarte[carteRetournee].sorteCarte == sorte2Cartes || tabCarte[carteRetournee].sorteCarte == sorte1Carte)
-                                    ChangerCarteRetournee(ref tabCarte, index2Cartes, ref joueurPC, ref maxTableau, ref carteRetournee);
-                                else
-                                    ChangerCarte(ref tabCarte, index2Cartes, ref joueurPC, ref maxTableau);
-                                
-                            }
-                            break;
-                        case 3:
-                        {
-                            Console.WriteLine("Nombre Carte PC " + nombreTypeCarte);
-                            int plusPetiteCarte = DeterminerPlusPetiteCarte(joueurPC);
-                            int autreCarte1 = 1, autreCarte2 = 2;
-                            Console.WriteLine("plus petite carte " + plusPetiteCarte);
-                            Console.WriteLine("Nombre Carte PC " + nombreTypeCarte);
-                            if (plusPetiteCarte == 1)
-                            {
-                                autreCarte1 = 0;
-                                autreCarte2 = 2;
-                            }
-                            else if (plusPetiteCarte == 2)
-                            {
-                                autreCarte1 = 0;
-                                autreCarte2 = 1;
-                            }
-                            if (tabCarte[carteRetournee].sorteCarte == joueurPC.mesCartes[autreCarte1].sorteCarte || tabCarte[carteRetournee].sorteCarte == joueurPC.mesCartes[autreCarte2].sorteCarte)
-                                ChangerCarteRetournee(ref tabCarte, plusPetiteCarte, ref joueurPC, ref maxTableau, ref carteRetournee);
-                            else
-                                ChangerCarte(ref tabCarte, plusPetiteCarte, ref joueurPC, ref maxTableau);
-                            break;
-                            }
-                        default:
-                            break;
+                        Console.WriteLine(i + 1 + " - sorte : " + joueur1.mesCartes[i].sorteCarte.ToString() + " valeur : " + joueur1.mesCartes[i].valeurJeu);
                     }
+                    Console.WriteLine(" Carte retournee sorte : " + tabCarte[carteRetournee].sorteCarte.ToString() + " valeur : " + tabCarte[carteRetournee].valeurJeu);
+                    int choix;
+                    Console.WriteLine("Que voulez vous faire : \n 1 - prendre une nouvelle carte \n 2 - Prendre la carte retournee \n 3 - Fin de cette manche  (Vous de avoir une jeu qui vaut au moins 21 !!!)");
+                    choix = Convert.ToInt32(Console.ReadLine());
+                    switch (choix)
+                    {
+
+                        case 1:
+                            {
+                                Console.WriteLine("Quelle carte souhaitez vous changer 1, 2 ou 3 : ");
+                                carteChanger = Convert.ToInt32(Console.ReadLine());
+                                ChangerCarte(ref tabCarte, carteChanger - 1, ref joueur1, ref maxTableau);
+                                break;
+                            }
+                        case 2:
+                            {
+                                Console.WriteLine("Quelle carte souhaitez vous changer 1, 2 ou 3 : ");
+                                carteChanger = Convert.ToInt32(Console.ReadLine());
+                                ChangerCarteRetournee(ref tabCarte, carteChanger - 1, ref joueur1, ref maxTableau, ref carteRetournee);
+                                break;
+                            }
+                        case 3:
+                            {
+                                Console.WriteLine("Votre jeu de carte vaut : " + CalculerValeurCartes(joueur1));
+                                if (CalculerValeurCartes(joueur1) < 21)
+                                    Console.WriteLine("votre jeu ne vaut pas au moins 21, vous ne pouvez pas mettre fin a ce tour  ");
+                                else
+                                {
+                                    Console.WriteLine("Fin de ce tour  ");
+                                    nouveauTour = false;
+                                }
+                                break;
+                            }
+                        default: break;
+                    }
+                    if (dernierTour == true)
+                    {
+                        nouveauTour = false;
+                    }
+                    //Au tour de l'ordinateur de jouer
+                    /* Carte de l'ordinateur...
+                    Console.WriteLine(" LES CARTES DE JOUEURPC : ");
+                    for (int i = 0; i < 3; i++)
+                    {
+                        Console.WriteLine(i + 1 + " - sorte : " + joueurPC.mesCartes[i].sorteCarte.ToString() + " valeur : " + joueurPC.mesCartes[i].valeurJeu);
+                    }*/
+                    if (CalculerValeurCartes(joueurPC) < 21 && dernierTour == false)
+                    {
+                        nombreTypeCarte = CalculerNombreTypeCarte(joueurPC);
+
+                        switch (nombreTypeCarte)
+                        {
+                            case 1: //Les trois carte du PC sont de meme sorte...
+                                {
+                                    int plusPetiteCarte = DeterminerPlusPetiteCarte(joueurPC);
+                                    int autreCarte1 = 1, autreCarte2 = 2;
+                                    Console.WriteLine("plus petite carte " + plusPetiteCarte);
+                                    Console.WriteLine("Nombre Carte PC " + nombreTypeCarte);
+                                    if (plusPetiteCarte == 1)
+                                    {
+                                        autreCarte1 = 0;
+                                        autreCarte2 = 2;
+                                    }
+                                    else if (plusPetiteCarte == 2)
+                                    {
+                                        autreCarte1 = 0;
+                                        autreCarte2 = 1;
+                                    }
+                                    if (tabCarte[carteRetournee].sorteCarte == joueurPC.mesCartes[autreCarte1].sorteCarte || tabCarte[carteRetournee].sorteCarte == joueurPC.mesCartes[autreCarte2].sorteCarte)
+                                        ChangerCarteRetournee(ref tabCarte, plusPetiteCarte, ref joueurPC, ref maxTableau, ref carteRetournee);
+                                    else
+                                        ChangerCarte(ref tabCarte, plusPetiteCarte, ref joueurPC, ref maxTableau);
+                                    break;
+                                }
+                            case 2: //deux sortes de cartes
+                                Sorte sorte2Cartes = Sorte2Cartes(joueurPC), sorte1Carte = Sorte1Carte(joueurPC);
+                                int valeur2Cartes = ValeurCartes(joueurPC, sorte2Cartes), valeur1Carte = ValeurCartes(joueurPC, sorte1Carte);
+                                int index1Carte = Index1Carte(joueurPC, sorte1Carte), index2Cartes = Index2Cartes(joueurPC, sorte2Cartes);
+                                if (valeur2Cartes > valeur1Carte)
+                                {
+                                    if (sorte2Cartes == tabCarte[carteRetournee].sorteCarte)
+                                        ChangerCarteRetournee(ref tabCarte, index1Carte, ref joueurPC, ref maxTableau, ref carteRetournee);
+                                    else
+                                        ChangerCarte(ref tabCarte, index1Carte, ref joueurPC, ref maxTableau);
+                                }
+                                else
+                                {
+                                    if (tabCarte[carteRetournee].sorteCarte == sorte2Cartes || tabCarte[carteRetournee].sorteCarte == sorte1Carte)
+                                        ChangerCarteRetournee(ref tabCarte, index2Cartes, ref joueurPC, ref maxTableau, ref carteRetournee);
+                                    else
+                                        ChangerCarte(ref tabCarte, index2Cartes, ref joueurPC, ref maxTableau);
+                                }
+                                break;
+                            case 3: // les cartes sont de 3 sortes
+                                {
+                                    int plusPetiteCarte = DeterminerPlusPetiteCarte(joueurPC);
+                                    int autreCarte1 = 1, autreCarte2 = 2;
+                                    if (plusPetiteCarte == 1)
+                                    {
+                                        autreCarte1 = 0;
+                                        autreCarte2 = 2;
+                                    }
+                                    else if (plusPetiteCarte == 2)
+                                    {
+                                        autreCarte1 = 0;
+                                        autreCarte2 = 1;
+                                    }
+                                    if (tabCarte[carteRetournee].sorteCarte == joueurPC.mesCartes[autreCarte1].sorteCarte || tabCarte[carteRetournee].sorteCarte == joueurPC.mesCartes[autreCarte2].sorteCarte)
+                                        ChangerCarteRetournee(ref tabCarte, plusPetiteCarte, ref joueurPC, ref maxTableau, ref carteRetournee);
+                                    else
+                                        ChangerCarte(ref tabCarte, plusPetiteCarte, ref joueurPC, ref maxTableau);
+                                    break;
+                                }
+                            default:
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        if (dernierTour == false || nouveauTour != false) Console.WriteLine("VOTRE ADVERSAIRE SIGNALE LA FIN DE CETTE MANCHE !!!");
+                        dernierTour = true;
+                    }
+                } while (nouveauTour == true && maxTableau > 1);
+
+                Console.WriteLine("ton jeu vaut : " + CalculerValeurCartes(joueur1) + " celui du pc vaut : " + CalculerValeurCartes(joueurPC));
+                if (CalculerValeurCartes(joueur1) > CalculerValeurCartes(joueurPC))
+                {
+                    Console.WriteLine(" BRAVO VOUS GAGNEZ CETTE MANCHE !!!");
+                    joueurPC.nbreVie--;
+                }
+                else if (CalculerValeurCartes(joueur1) < CalculerValeurCartes(joueurPC))
+                {
+                    Console.WriteLine(" VOUS AVEZ PERDUE CETTE MANCHE !!!");
+                    joueur1.nbreVie--;
                 }
                 else
                 {
-
+                    Console.WriteLine(" VOUS AVEZ LE MEME NOMBRE DE POINTS !!!");
                 }
-            } while (nouveauTour==true && maxTableau >1);
+                Console.WriteLine("tes vie : " + joueur1.nbreVie + " vie du pc: " + joueurPC.nbreVie);
+                if (joueur1.nbreVie == 0 || joueurPC.nbreVie == 0) nouvelleManche = false;
+            } while (nouvelleManche);
+            if(joueur1.nbreVie==0) Console.WriteLine(" VOUS AVEZ PERDUE CETTE PARTIE !!!");
+            else Console.WriteLine(" VOUS AVEZ GAGNE CETTE  PARTIE !!!");
+
         }
     }
 }
