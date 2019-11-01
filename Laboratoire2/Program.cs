90e2a75a86ff77edc3fc00d40ccea1174ff2c899
lonfa1330100 <54541484+lonfa1330100@users.noreply.github.com>
2019-11-01 08:46:49 -0400

    l

--- Laboratoire2/Program.cs
+++ Laboratoire2/Program.cs
@@ -25,11 +25,11 @@ namespace Laboratoire2
         }
         public struct Joueur
         {
-            public int[] mesCartes;
+            public Carte[] mesCartes;
             public int nbreVie;
-            public Joueur(int carte1, int carte2, int carte3) : this()
+            public Joueur(Carte carte1, Carte carte2, Carte carte3) : this()
             {
-                mesCartes = new int[3];
+                mesCartes = new Carte[3];
                 nbreVie = 3;
                 mesCartes[0] = carte1;
                 mesCartes[1] = carte2;
@@ -54,23 +54,26 @@ namespace Laboratoire2
                     k++;
                 }
             }
-        }
-        static public void DistribuerCarte(ref Joueur joueur1, ref Joueur joueurPC, ref Carte[] tabCarte,ref int maxTableau)
+        } 
+        static public void DistribuerCarte(ref Joueur joueur1, ref Joueur joueurPC,ref Carte[] tabCarte,ref int maxTableau)
         {
             int carteSelect;
-            for (int i=0; i<3; i++)
+            for (int i = 0; i < 3; i++)
             {
                 carteSelect = (int)generateurNb.Next(0, maxTableau + 1);
-                joueur1.mesCartes[i] = maxTableau;
-                maxTableau--;
                 PermuterCarte(ref tabCarte, carteSelect, maxTableau);
+                joueur1.mesCartes[i] = tabCarte[maxTableau];
+                maxTableau--;
+                
 
                 carteSelect = (int)generateurNb.Next(0, maxTableau + 1);
-                joueurPC.mesCartes[i] = maxTableau;
-                maxTableau--;
                 PermuterCarte(ref tabCarte, carteSelect, maxTableau);
+                joueurPC.mesCartes[i] = tabCarte[maxTableau];
+                maxTableau--;
+                
             }
         }
+        
         static public void PermuterCarte(ref Carte[] tabCarte, int carteSelect, int maxTableau)
         {
             Carte carteTemporaire;
@@ -78,32 +81,32 @@ namespace Laboratoire2
             tabCarte[carteSelect] = tabCarte[maxTableau];
             tabCarte[maxTableau] = carteTemporaire;
         }
-        static public void ChangerCarte( ref Carte[] tabCarte, int carteChanger, ref Joueur Joueur, ref int maxTableau )
+        static public void ChangerCarte(ref Carte[] tabCarte, int carteChanger, ref Joueur Joueur, ref int maxTableau)
         {
             int carteSelectionnee;
-            carteSelectionnee = (int)generateurNb.Next(0, maxTableau+1);
+            carteSelectionnee = (int)generateurNb.Next(0, maxTableau + 1);
             PermuterCarte(ref tabCarte, carteSelectionnee, maxTableau);
-            Joueur.mesCartes[carteChanger] = maxTableau;
+            Joueur.mesCartes[carteChanger] = tabCarte[maxTableau];
             maxTableau--;
 
         }
         static public void ChangerCarteRetournee(ref Carte[] tabCarte, int carteChanger, ref Joueur Joueur, ref int maxTableau, ref int carteRetournee)
         {
             int carteSelectionnee;
-            Joueur.mesCartes[carteChanger] = carteRetournee;
-            carteSelectionnee = (int)generateurNb.Next(0, maxTableau);
+            Joueur.mesCartes[carteChanger] = tabCarte[carteRetournee];
+            carteSelectionnee = (int)generateurNb.Next(0, maxTableau+1);
             PermuterCarte(ref tabCarte, carteSelectionnee, maxTableau);
             carteRetournee = maxTableau;
             maxTableau--;
         }
-        static public int CalculerValeurCartes(Carte[] tabCarte, Joueur joueur)
+        static public int CalculerValeurCartes( Joueur joueur)
         {
-            int valeurCarte = 0;
+            int valeurCarte ;
             int[] tabValeur = { 0, 0, 0, 0 };
             for (int i = 0; i < 3; i++)
             {
-                int a =   (int)tabCarte[joueur.mesCartes[i]].sorteCarte;
-                int b = tabCarte[joueur.mesCartes[i]].valeurJeu;
+                int a = (int)joueur.mesCartes[i].sorteCarte;
+                int b = joueur.mesCartes[i].valeurJeu;
                 tabValeur[a-1] = tabValeur[a - 1] + b;
             }
             valeurCarte = tabValeur[0];
@@ -114,13 +117,13 @@ namespace Laboratoire2
             }
                 return valeurCarte;
         }
-        static public int CalculerNombreTypeCarte(Carte[] tabCarte, Joueur joueurPC)
+        static public int CalculerNombreTypeCarte(Joueur joueurPC)
         {
             int nmbre = 1;
             Sorte sorte1, sorte2, sorte3;
-            sorte1 = tabCarte[joueurPC.mesCartes[0]].sorteCarte;
-            sorte2 = tabCarte[joueurPC.mesCartes[1]].sorteCarte;
-            sorte3 = tabCarte[joueurPC.mesCartes[2]].sorteCarte;
+            sorte1 = joueurPC.mesCartes[0].sorteCarte;
+            sorte2 = joueurPC.mesCartes[1].sorteCarte;
+            sorte3 = joueurPC.mesCartes[2].sorteCarte;
 
             if (sorte2 != sorte1)
                 nmbre++;
@@ -130,26 +133,108 @@ namespace Laboratoire2
 
             return nmbre;
         }
-        static public int DeterminerPlusPetiteCarte(Carte[] tabCarte, Joueur joueurPC)
+        static public int DeterminerPlusPetiteCarte(Joueur joueurPC)
         {
             int pluspetite=0;
             Carte carte1, carte2, carte3;
-            carte1 = tabCarte[joueurPC.mesCartes[0]];
-            carte2 = tabCarte[joueurPC.mesCartes[1]];
-            carte3 = tabCarte[joueurPC.mesCartes[2]];
-            if (carte2.valeurJeu > carte1.valeurJeu && carte2.valeurJeu > carte3.valeurJeu)
+            carte1 = joueurPC.mesCartes[0];
+            carte2 = joueurPC.mesCartes[1];
+            carte3 = joueurPC.mesCartes[2];
+            if (carte2.valeurJeu < carte1.valeurJeu && carte2.valeurJeu < carte3.valeurJeu)
                 pluspetite = 1;
 
-             else if (carte3.valeurJeu > carte1.valeurJeu && carte3.valeurJeu > carte2.valeurJeu)
+             else if (carte3.valeurJeu < carte1.valeurJeu && carte3.valeurJeu < carte2.valeurJeu)
                 pluspetite = 2;
 
-            return joueurPC.mesCartes[pluspetite];
+            return pluspetite;
+        }
+        static public Sorte Sorte2Cartes(Joueur joueur)
+        {
+            Sorte sorte2Cartes = (Sorte)1;
+            int[] nombreCarteParSorte = { 0, 0, 0, 0 };
+            for (int i = 0; i < 3; i++)
+            {
+                int a = (int)joueur.mesCartes[i].sorteCarte;
+                nombreCarteParSorte[a - 1] = nombreCarteParSorte[a - 1] + 1;
+            }
+
+            int k = 0;
+            while (nombreCarteParSorte[k] != 2)
+            {
+                k++;
+                if (nombreCarteParSorte[k] == 2) sorte2Cartes = (Sorte)(k+1);
+                
+            } 
+            return sorte2Cartes;
+        }
+        static public Sorte Sorte1Carte(Joueur joueur)
+        {
+            Sorte sorte1Carte = (Sorte)1;
+            int[] nombreCarteParSorte = { 0, 0, 0, 0 };
+            for (int i = 0; i < 3; i++)
+            {
+                int a = (int)joueur.mesCartes[i].sorteCarte;
+                nombreCarteParSorte[a-1] = nombreCarteParSorte[a - 1] + 1;
+            }
+
+            int k = 0;
+            while(nombreCarteParSorte[k] != 1)
+            {
+                k++;
+                if (nombreCarteParSorte[k] == 1) sorte1Carte = (Sorte)(k + 1);
+            } 
+            return sorte1Carte;
+        }
+        static public int ValeurCartes(Joueur joueurPC, Sorte sorte)
+        {
+            int valeur = 0;
+            for(int i=0; i<3; i++)
+            {
+                if(joueurPC.mesCartes[i].sorteCarte == sorte)
+                {
+                    valeur += joueurPC.mesCartes[i].valeurJeu;
+                }
+            }
+            return valeur;
+        }
+        static public int Index1Carte(Joueur joueurPC, Sorte sorte1Carte)
+        {
+            int index;
+            if (joueurPC.mesCartes[0].sorteCarte == sorte1Carte)
+            {
+                index = 0;
+            }
+            else if (joueurPC.mesCartes[1].sorteCarte == sorte1Carte)
+                index = 1;
+            else index = 2;
+
+            return index;
+        }
+        static public int Index2Cartes(Joueur joueurPC, Sorte sorte2Cartes)
+        {
+            int index = 0,  val=12;
+            
+            for(int i=0; i<3; i++)
+            {
+                if(joueurPC.mesCartes[i].sorteCarte == sorte2Cartes)
+                {
+                    if (joueurPC.mesCartes[i].valeurJeu < val)
+                    {
+                        val = joueurPC.mesCartes[i].valeurJeu;
+                        index = i;
+                    }
+                }
+
+            }
+              
+            return index;
         }
         static void Main(string[] args)
         { 
             Carte[] tabCarte = new Carte[52];
             //bool partie = true;
             Joueur joueur1,joueurPC;
+            Carte initCarte = new Carte(0,0,0);  //pour initialiser mes joueurs
             int maxTableau;
             int carteRetournee;
             int carteChanger;
@@ -157,30 +242,33 @@ namespace Laboratoire2
             int nombreTypeCarte;
 
             
-            joueur1 = new Joueur(0, 0, 0);
-            joueurPC = new Joueur(0, 0, 0);
+            joueur1 = new Joueur(initCarte, initCarte, initCarte);
+            joueurPC = new Joueur(initCarte, initCarte, initCarte);
 
             
-                InitialierTabCarte(ref tabCarte);
-                maxTableau = 51;
-                DistribuerCarte(ref joueur1, ref joueurPC, ref tabCarte, ref maxTableau);
-                carteRetournee = Convert.ToInt32(generateurNb.Next(0, maxTableau + 1));
-                PermuterCarte(ref tabCarte, carteRetournee, maxTableau);
-                carteRetournee = maxTableau;
-                maxTableau--;
+            InitialierTabCarte(ref tabCarte);
+            maxTableau = 51;
+            
+            DistribuerCarte(ref joueur1, ref joueurPC, ref tabCarte, ref maxTableau);
+            
+            carteRetournee = Convert.ToInt32(generateurNb.Next(0, maxTableau + 1));
+            PermuterCarte(ref tabCarte, carteRetournee, maxTableau);
+            carteRetournee = maxTableau;
+            maxTableau--;
+            
             do
             {
                 Console.WriteLine(" LES CARTES DE JOUEUR1 : ");
                 for (int i = 0; i < 3; i++)
                 {
-                    Console.WriteLine(i + 1 + " - sorte : " + tabCarte[joueur1.mesCartes[i]].sorteCarte.ToString() + " valeur : " + tabCarte[joueur1.mesCartes[i]].valeur);
+                    Console.WriteLine(i + 1 + " - sorte : " + joueur1.mesCartes[i].sorteCarte.ToString() + " valeur : " + joueur1.mesCartes[i].valeurJeu);
                 }
                 Console.WriteLine(" LES CARTES DE JOUEURPC : ");
                 for (int i = 0; i < 3; i++)
                 {
-                    Console.WriteLine(i + 1 + " - sorte : " + tabCarte[joueurPC.mesCartes[i]].sorteCarte.ToString() + " valeur : " + tabCarte[joueurPC.mesCartes[i]].valeur);
+                    Console.WriteLine(i + 1 + " - sorte : " + joueurPC.mesCartes[i].sorteCarte.ToString() + " valeur : " + joueurPC.mesCartes[i].valeurJeu);
                 }
-                Console.WriteLine(" Carte retournee sorte : " + tabCarte[carteRetournee].sorteCarte.ToString() + " valeur : " + tabCarte[carteRetournee].valeur);
+                Console.WriteLine(" Carte retournee sorte : " + tabCarte[carteRetournee].sorteCarte.ToString() + " valeur : " + tabCarte[carteRetournee].valeurJeu);
                 int choix;
 
                 
@@ -189,6 +277,7 @@ namespace Laboratoire2
                 choix = Convert.ToInt32(Console.ReadLine());
                 switch (choix)
                 {
+                    
                     case 1:
                         {
                             Console.WriteLine("Quelle carte souhaitez vous changer 1, 2 ou 3 : ");
@@ -205,8 +294,8 @@ namespace Laboratoire2
                         }
                     case 3:
                         {
-                            Console.WriteLine("Votre jeu de carte vaut : " + CalculerValeurCartes(tabCarte, joueur1));
-                            if (CalculerValeurCartes(tabCarte, joueur1) < 21)
+                            Console.WriteLine("Votre jeu de carte vaut : " + CalculerValeurCartes( joueur1));
+                            if (CalculerValeurCartes(joueur1) < 21)
                                 Console.WriteLine("votre jeu ne vaut pas au moins 21, vous ne pouvez pas mettre fin a ce tour  ");
                             else
                             {
@@ -220,21 +309,83 @@ namespace Laboratoire2
                 //Au tour de l'ordinateur de jouer
                 if (true) //CalculerValeurCartes(tabCarte, joueurPC) < 21
                 {
-                    nombreTypeCarte = CalculerNombreTypeCarte(tabCarte, joueurPC);
+                    nombreTypeCarte = CalculerNombreTypeCarte(joueurPC);
                    
                     switch (nombreTypeCarte)
                     {
                         case 1: //Les trois carte du PC sont de meme sorte...
+                        {
+                            int plusPetiteCarte = DeterminerPlusPetiteCarte(joueurPC);
+                            int autreCarte1 = 1, autreCarte2 = 2;
+                            Console.WriteLine("plus petite carte " + plusPetiteCarte);
                             Console.WriteLine("Nombre Carte PC " + nombreTypeCarte);
-                            int plusPetiteCarte = DeterminerPlusPetiteCarte(tabCarte, joueurPC);
-                            Console.WriteLine("plus petite carte " + tabCarte[plusPetiteCarte]);
+                            if (plusPetiteCarte == 1)
+                            {
+                                autreCarte1 = 0;
+                                autreCarte2 = 2;
+                            }
+                            else if (plusPetiteCarte == 2)
+                            {
+                                autreCarte1 = 0;
+                                autreCarte2 = 1;
+                            }
+                            if (tabCarte[carteRetournee].sorteCarte == joueurPC.mesCartes[autreCarte1].sorteCarte || tabCarte[carteRetournee].sorteCarte == joueurPC.mesCartes[autreCarte2].sorteCarte)
+                                ChangerCarteRetournee(ref tabCarte, plusPetiteCarte,ref joueurPC, ref maxTableau, ref carteRetournee);
+                            else
+                                ChangerCarte(ref tabCarte, plusPetiteCarte, ref joueurPC, ref maxTableau);
                             break;
+                        }
+                            
                         case 2:
                             Console.WriteLine("Nombre Carte PC " + nombreTypeCarte);
+                            
+                            Sorte sorte2Cartes = Sorte2Cartes(joueurPC), sorte1Carte = Sorte1Carte(joueurPC);
+                            int valeur2Cartes = ValeurCartes(joueurPC, sorte2Cartes), valeur1Carte = ValeurCartes(joueurPC, sorte1Carte);
+                            int index1Carte =Index1Carte(joueurPC, sorte1Carte), index2Cartes =Index2Cartes(joueurPC, sorte2Cartes) ;
+                            Console.WriteLine("Sorte 2 Carte : " + sorte2Cartes + " valeur 2 cartes : " + valeur2Cartes);
+                            Console.WriteLine("Sorte 1 Carte : " + sorte1Carte + " valeur 1 cartes : " + valeur1Carte);
+                            Console.WriteLine("Index 1 Carte : " + index1Carte + " Index 2 Cartes : " + index2Cartes);
+                            if (valeur2Cartes > valeur1Carte)
+                            {
+                                if(sorte2Cartes == tabCarte[carteRetournee].sorteCarte)
+                                    ChangerCarteRetournee(ref tabCarte, index1Carte, ref joueurPC, ref maxTableau, ref carteRetournee);
+                                
+                                else
+                                    ChangerCarte(ref tabCarte, index1Carte, ref joueurPC, ref maxTableau);
+                                
+                            }
+                            else
+                            {
+                                if (tabCarte[carteRetournee].sorteCarte == sorte2Cartes || tabCarte[carteRetournee].sorteCarte == sorte1Carte)
+                                    ChangerCarteRetournee(ref tabCarte, index2Cartes, ref joueurPC, ref maxTableau, ref carteRetournee);
+                                else
+                                    ChangerCarte(ref tabCarte, index2Cartes, ref joueurPC, ref maxTableau);
+                                
+                            }
                             break;
                         case 3:
+                        {
                             Console.WriteLine("Nombre Carte PC " + nombreTypeCarte);
+                            int plusPetiteCarte = DeterminerPlusPetiteCarte(joueurPC);
+                            int autreCarte1 = 1, autreCarte2 = 2;
+                            Console.WriteLine("plus petite carte " + plusPetiteCarte);
+                            Console.WriteLine("Nombre Carte PC " + nombreTypeCarte);
+                            if (plusPetiteCarte == 1)
+                            {
+                                autreCarte1 = 0;
+                                autreCarte2 = 2;
+                            }
+                            else if (plusPetiteCarte == 2)
+                            {
+                                autreCarte1 = 0;
+                                autreCarte2 = 1;
+                            }
+                            if (tabCarte[carteRetournee].sorteCarte == joueurPC.mesCartes[autreCarte1].sorteCarte || tabCarte[carteRetournee].sorteCarte == joueurPC.mesCartes[autreCarte2].sorteCarte)
+                                ChangerCarteRetournee(ref tabCarte, plusPetiteCarte, ref joueurPC, ref maxTableau, ref carteRetournee);
+                            else
+                                ChangerCarte(ref tabCarte, plusPetiteCarte, ref joueurPC, ref maxTableau);
                             break;
+                            }
                         default:
                             break;
                     }
@@ -243,7 +394,6 @@ namespace Laboratoire2
                 {
 
                 }
-
             } while (nouveauTour==true && maxTableau >1);
         }
     }
